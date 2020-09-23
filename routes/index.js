aeb52f5b9e1bd8be5cefd58acfc503f899305859
Nadav <nadav.nassi@gmail.com>
2020-09-23 07:11:28 +0000

    add register message

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,7 @@ router.post("/register", function(req, res){
 			return res.render("register", {"error": err.message});
 		}
 		passport.authenticate("local")(req, res, function(){
+			req.flash("success", "Welcome " + req.user.username);
 			res.redirect("/campgrounds");
 		});
 	});
